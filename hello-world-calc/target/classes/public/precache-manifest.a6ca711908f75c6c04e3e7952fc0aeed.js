ac1905b084711034db559d093d3d7db8417d2720
Jurgita <Jurgita.pupalaigiene@gmail.com>
2018-12-06 14:18:57 +0200

    po sestos paskaitos

---
+++ hello-world-calc/target/classes/public/precache-manifest.a6ca711908f75c6c04e3e7952fc0aeed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82ef5311977cbac13976cf58a7b991ef",
+    "url": "./static/media/index.82ef5311.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "df92e2c31c4b7178b723",
+    "url": "./static/js/main.df92e2c3.chunk.js"
+  },
+  {
+    "revision": "94acb358292a69bd02a3",
+    "url": "./static/js/1.94acb358.chunk.js"
+  },
+  {
+    "revision": "df92e2c31c4b7178b723",
+    "url": "./static/css/main.6471f8ac.chunk.css"
+  },
+  {
+    "revision": "94acb358292a69bd02a3",
+    "url": "./static/css/1.59290760.chunk.css"
+  },
+  {
+    "revision": "8258a6ae1dfee8bbe02859adc1becacd",
+    "url": "./index.html"
+  }
+];
